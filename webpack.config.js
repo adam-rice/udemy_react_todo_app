0d5216a95f71ecd232c74849a4d926f0b21f3a20
Adam Rice <adamcoltonrice@gmail.com>
2016-12-31 10:09:58 -0700

    necessary files

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  entry: './app/app.jsx',
+  output: {
+    path: __dirname,
+    filename: './public/bundle.js'
+  },
+  resolve: {
+    root: __dirname,
+    alias: {
+    },
+    extenstions: ['', '.js', '.jsx', '.json', '.scss', '.css'],
+  },
+  module: {
+    loaders: [
+      {
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015', 'es2016', 'es2017'],
+        },
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/
+      },
+      { test: /\.css$/, loader: 'style!css' },
+      { test: /\.scss$/, loader: 'style!css!sass' },
+    ]
+  },
+  devtool: 'cheap-module-eval-source-map'
+};
