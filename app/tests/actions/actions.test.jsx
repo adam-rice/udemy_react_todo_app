be7ae275a0ffb57b602d9f621a4c66fdf8110644
Adam Rice <adamcoltonrice@gmail.com>
2017-01-21 15:33:18 -0700

    add redux and setup action dir with four new action gens

---
+++ app/tests/actions/actions.test.jsx
@@ -0,0 +1,16 @@
+/*jshint esversion: 6 */
+
+// import expect from 'expect';
+// import actions from '../../app/actions.jsx';
+//
+// describe('Actions', () => {
+//   it('should generate search text action', () => {
+//     var action = {
+//       type: "SET_SEARCH_TEXT",
+//       searchText: 'something'
+//     };
+//     var res = actions.setSearchText(action.searchText);
+//
+//     expect(res).toEqual(action);
+//   });
+// });
